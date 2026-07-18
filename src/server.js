ce2ed928cbb16962695c00463572869851e90637
Felipe Porcelli <felipe.porcelli@medcof.tech>
2026-07-18 15:55:05 -0300

    :wrench: Adds cors

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from "express"
+import cors from "cors"
 import {requireAuth} from "./middleware/authMiddleware.js"
 import { connectMongoDB } from "./config/db.js"
 import authRouter from "./routes/authRouter.js"
@@ -13,6 +14,12 @@ const app = express();
 const port = 3000;
 
 app.use(express.json())
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+  ],
+  credentials: true
+}))
 app.use("/api/auth", authRouter)
 app.use("/api/user", userRouter)
 app.use("/api/reviews", reviewRouter)
